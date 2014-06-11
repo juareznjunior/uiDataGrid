ddb9a66bb1bc6c20bbb84383f7bebfe60fccd490
Juarez Junior <juareznjunior@gmail.com>
2014-06-11 17:36:12 -0300

    add createToolBarButtons and scope global in directive

--- angular-datagrid/datagrid.js
+++ angular-datagrid/datagrid.js
@@ -1,85 +1,97 @@
+/**
+ * [description]
+ * @param  {[type]} ng [description]
+ * @return {[type]}    [description]
+ *
+ * Beta
+ */
 ;(function(ng){
 
 	'use strict';
 
-	var  ngElem = function(elem) { return ng.element(elem); }
-		,noop = function() {}
-		,watchers = function() {
-			var self = this;
-			
-			this.$scope.$watch('title',function(n,o){
-				if ( n !== o ) {
-					self.$title.text(n);
-				}
-			});
-			
-			if ( false === this.$scope.autoLoad ) {
-				this.$scope.$watch('autoLoad',function(n){
-					if ( true === n ) {
-						self.createRows();
-						noop();
-					}
-				});
-			}
-		}
+	var uiDataGrid = function(options) {
 
-	var uiDataGrid = function($scope,$element,$attrs,$parse) {
-	
-		this.$scope   = $scope;
-		this.$element = $element;
-		this.$title   = $element.children().eq(0);
+		options = {
+			 title  : options.title || 'DataGrid Title'
+			,height : Number(options.height.replace(/[^0-9.]/,'')) || 200
+			,element: options.element
+		};
+
+		// title
+		this.elementTitle = options.element.children().eq(0).text( options.title );
 		
 		// helpers
-		var uiDataGridTables = [].slice.call($element[0].getElementsByTagName('table'))
-			,contentScroll = uiDataGridTables.shift().parentNode.parentNode
-			,tableHeaderChilds = ngElem(uiDataGridTables[0]).children()
-			,tableGridLayout = ngElem(uiDataGridTables[1]).children()
-			,tableBodyChilds = ngElem(uiDataGridTables[2]).children()
-			,scrollHeight = [($scope.height || '200'),'px'].join('');
+		var uiDataGridTables  = [].slice.call(options.element[0].getElementsByTagName('table'))
+			,contentScroll     = uiDataGridTables.shift().parentNode.parentNode
+			,tableHeaderChilds = ng.element(uiDataGridTables[0]).children()
+			,tableGridLayout   = ng.element(uiDataGridTables[1]).children()
+			,tableBodyChilds   = ng.element(uiDataGridTables[2]).children()
+			,scrollHeight      = [options.height,'px'].join('');
 			
 		// grid layout
-		this.uiDataGridColGroupLayout = ngElem(tableGridLayout[0]);
-		this.uiDataGridTbodyLayout = ngElem(tableGridLayout[1]);
+		this.uiDataGridColGroupLayout = ng.element(tableGridLayout[0]);
+		this.uiDataGridTbodyLayout    = ng.element(tableGridLayout[1]);
 		
 		// header
-		this.uiDataGridColGroupHeader = ngElem(tableHeaderChilds[0]);
-		this.uiDataGridTheadHeader = ngElem(tableHeaderChilds[1]);
+		this.uiDataGridColGroupHeader = ng.element(tableHeaderChilds[0]);
+		this.uiDataGridTheadHeader    = ng.element(tableHeaderChilds[1]);
 		
 		// body
-		this.uiDataGridColGroupBody = ngElem(tableBodyChilds[0]);
-		this.uiDataGridTheadBody = ngElem(tableBodyChilds[1]);
-		this.uiDataGridTbody = ngElem(tableBodyChilds[2]);
+		this.uiDataGridColGroupBody = ng.element(tableBodyChilds[0]);
+		this.uiDataGridTheadBody    = ng.element(tableBodyChilds[1]);
+		this.uiDataGridTbody        = ng.element(tableBodyChilds[2]);
+
+		// tools
+		this.uiDataGridTools = ng.element(uiDataGridTables[3].tBodies[0].rows[0].cells);
 
 		// scroll height
-		ngElem(uiDataGridTables[2].parentNode).css({'height':scrollHeight});
+		ng.element(uiDataGridTables[2].parentNode).css({'height':scrollHeight});
 		
 		// clean helpers
-		uiDataGridTables = tableHeaderChilds = tableBodyChilds = null;
-		
-		watchers.call(this);
-		
-		this.createColumns();
+		uiDataGridTables = tableHeaderChilds = tableBodyChilds = options = null;
 
-		// tbody event closest tr
-		// click-row
-		if ( ng.isFunction($scope.clickRow) ) {
-			this.uiDataGridTbody.on('click',function(e){
-				$scope.clickRow({'row':e.target.parentNode});
-			});
+		// options
+		this.options = {
+			 title   : 'DataGrid Title'
+			,columns : []
+			,clickRow: false
+			,autoload: true
+			,buttons : [] 
+		};
+	};
+
+	uiDataGrid.prototype = {
+		config: function(options) {
+
+			this.options = ng.extend({},this.options,options || {});
+
+			if ( ng.isFunction(this.options.clickRow) ) {
+				this.uiDataGridTbody.data('udg-callback',this.options.clickRow).on('click',function(e){
+					ng.element(e.currentTarget).data('udg-callback').call(e.target.parentNode);
+				});
+			}
+
+			createColumns.call(this);
+			createToolBarButtons.call(this);
+		}
+		,setTitle : function(title) {
+			this.elementTitle.text(title);
+		}
+		,load: function() {
+			this.options.autoload = true;
+			createRows.call(this);
 		}
 	}
-	uiDataGrid.prototype = {
-		createColumns: function() {
-		
-			if ( undefined === this.$scope.columns ) {
-				return this;
-			}
+
+	// private
+	var 
+		createColumns = function() {
 			
 			var  auxTh
 				,auxCol
 				,obj
 				,title
-				,len = this.$scope.columns.length
+				,len = this.options.columns.length
 				,i  = 0
 				,sw = 0
 				,al = 'ui-datagrid-align-'
@@ -87,9 +99,9 @@
 				,colTpl = '<col></col>'
 				,thTpl  = '<th class="ui-widget ui-state-default"></th>'
 				,tdTpl  = '<td class="ui-widget ui-widget-content"></td>'
-				,tr1 = ngElem(this.uiDataGridTbodyLayout[0].rows[0]).empty()
-				,tr2 = ngElem(this.uiDataGridTheadHeader[0].rows[0]).empty()
-				,tr3 = ngElem(this.uiDataGridTheadBody[0].rows[0]).empty();
+				,tr1 = ng.element(this.uiDataGridTbodyLayout[0].rows[0]).empty()
+				,tr2 = ng.element(this.uiDataGridTheadHeader[0].rows[0]).empty()
+				,tr3 = ng.element(this.uiDataGridTheadBody[0].rows[0]).empty();
 
 			/**
 			 * Aux Function
@@ -114,23 +126,23 @@
 			}
 
 			// enable row number
-			if ( this.$scope.rowNumber ) {
+			if ( this.options.rowNumber ) {
 
-				auxTh  = ngElem(thTpl).addClass('ui-state-default ui-datagrid-cell-rownumber');
-				auxCol = ngElem(colTpl);
+				auxTh  = ng.element(thTpl).addClass('ui-state-default ui-datagrid-cell-rownumber');
+				auxCol = ng.element(colTpl);
 
 				appendElements.call(this,auxTh,auxCol);
 
 				// grid layout
-				tr1.append(ngElem('<td class="ui-widget ui-widget-content ui-datagrid-cell-rownumber"></td>'));
+				tr1.append(ng.element('<td class="ui-widget ui-widget-content ui-datagrid-cell-rownumber"></td>'));
 
 				sw = 20;
 			}
 			
-			for (; obj = this.$scope.columns[i]; i += 1) {
+			for (; obj = this.options.columns[i]; i += 1) {
 			
-				auxTh = ngElem(thTpl).text(obj.title || obj.name).addClass(al+(( /left|right|center/.test(obj.align) ) ? obj.align : 'left'));
-				auxCol = ngElem(colTpl);
+				auxTh = ng.element(thTpl).text(obj.title || obj.name).addClass(al+(( /left|right|center/.test(obj.align) ) ? obj.align : 'left'));
+				auxCol = ng.element(colTpl);
 				if ( (i+1) < len ) {
 					auxCol.css({"width":obj.width+'px'});
 				}
@@ -138,7 +150,7 @@
 				appendElements.call(this,auxTh,auxCol);
 
 				// grid layout
-				tr1.append(ngElem(tdTpl));
+				tr1.append(ng.element(tdTpl));
 				
 				sw += obj.width || 0;
 			}
@@ -148,12 +160,13 @@
 			auxTh = auxCol = null;
 			
 			this.uiDataGridTheadBody.addClass('ng-hide');
-			
-			return this;
+
+			( true === this.options.autoload && this.load() );
 		}
-		,createRows: function() {
-			if ( undefined === this.$scope.rows ) {
-				return this;
+		,createRows = function() {
+
+			if ( false === this.options.autoload ) {
+				return;
 			}
 			
 			var  i = 0
@@ -166,46 +179,76 @@
 				,theadThs = this.uiDataGridTheadHeader[0].rows[0].cells
 				,oTbody = this.uiDataGridTbody.empty()[0];
 			
-			for (; data = this.$scope.rows[i]; i += 1) {
+			for (; data = this.options.rows[i]; i += 1) {
 				row = oTbody.insertRow(-1);
-				ngElem(row).data('mapper-json',data);
+				ng.element(row).data('mapper-json',data);
 				
 				// row number
-				if ( this.$scope.rowNumber ) {
-					ngElem(row.insertCell(-1)).addClass('ui-state-default ui-datagrid-cell-rownumber ui-datagrid-align-center').text((i+1));
+				if ( this.options.rowNumber ) {
+					ng.element(row.insertCell(-1)).addClass('ui-state-default ui-datagrid-cell-rownumber ui-datagrid-align-center').text((i+1));
 				}
 
 				j = 0;
-				for (; json = this.$scope.columns[j]; j += 1) {
+				for (; json = this.options.columns[j]; j += 1) {
 					cell = row.insertCell(-1);
-					ngElem(cell).addClass(cls +' '+ theadThs[cell.cellIndex].className.split(' ').slice(-1)).html(data[json.name]);
+					ng.element(cell).addClass(cls +' '+ theadThs[cell.cellIndex].className.split(' ').slice(-1)).html(data[json.name]);
 				}
 			}
 			
 			data = row = json = cell = theadThs = oTbody = null;
-			
-			return this;
 		}
-	}
+		,createToolBarButtons = function() {
+
+			if ( ng.isArray( this.options.buttons ) ) {
+
+				// cell to append btns
+				var obj
+					,btn
+					,self = this
+					,cell = ng.element(this.uiDataGridTools[0])
+					,idx  = 0;
+
+				// each button
+				for (; obj = this.options.buttons[idx]; idx += 1) {
+
+					var btn = ng.element('<button type="button"></button>');
+
+					obj.text  = obj.label || obj.text;
+					obj.click = obj.fn || obj.click;
+
+					delete obj.fn;
+					delete obj.label;
+
+					if ( ng.isFunction(obj.click) ) {
+						btn.data('udg-callback',obj.click).on('click',function(e){
+							ng.element(e.currentTarget).data('udg-callback').call(self,e.currentTarget);
+						});
+					}
+
+					btn.text(obj.text).addClass('btn btn-default');
+
+					cell.append(btn[0]);
+				}
+
+				cell = btn = null;
+			}
+		};
 
 	ng.module('uiDataGrid', [])
-		.directive('uiDatagrid', function factory($parse) {
+		.directive('uiDatagrid', function factory() {
 			return {
 				 restrict: 'EA'
 				,templateUrl:'angular-datagrid/template/datagrid.html'
 				,replace:true
-				,scope: {
-					 columns   : '='
-					,rows      : '='
-					,autoLoad  : '=autoLoad'
-					,title     : '='
-					,clickRow  : '&clickRow'
-					,height    : '='
-					,rowNumber : '=rowNumber'
-				}
-				,compile: function($element,$attrs) {
-					return function($scope,$element,$attrs) {
-						new uiDataGrid($scope,$element,$attrs,$parse);
+				,compile: function($scope,$attrs) {
+					return function($scope,$element,$attrs){
+						$scope.datagrid = new uiDataGrid({
+							 element: $element
+							,height: $attrs.height || '200'
+							,title: $attrs.title
+						});
+
+						uiDataGrid = null;
 					}
 				}
 			};
